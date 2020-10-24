fd976a82bdbcb0111533ccfa76ed4f46b72acf00
violanes <theshablyko@gmail.com>
2020-10-24 12:30:54 +0300

    new controllers, dynamic menu

--- config.php
+++ config.php
@@ -4,9 +4,9 @@ $g_Settings = array
 (
     "db" => array
     (
-        "host" => "",
-        "username" => "",
+        "host" => "localhost",
+        "username" => "root",
         "password" => "",
-        "dbname" => "",
+        "dbname" => "hackathon",
     )
 );
