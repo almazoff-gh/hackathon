4cb7c34d0d062ead805a1f74abb1518522dcdd35
almazoff <yanak.kostya@yandex.ru>
2020-10-25 16:52:09 +0300

    New Mark System

--- includes/footer.php
+++ includes/footer.php
@@ -1,2 +1,12 @@
+<footer class="footer">
+    <div class="card" style="background: #e9ecef;">
+        <div class="container">
+            <div class="card-body" style="text-align: center">
+                <strong class="card-title">NStudy</strong>
+                <p class="card-text" style="font-size: 15px">Сделано с любовью к коду.</p>
+            </div>
+        </div>
+    </div>
+</footer>
 </body>
 </html>
